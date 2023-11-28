620740b90b8df7ff8c33ef861bd40383b57a7a1e
daniel-macias <josedanielmr@gmail.com>
2023-11-27 21:21:02 -0600

    in the process of chaning the project item component

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -14,43 +14,28 @@ const Projects = () => {
         <h2 className='py-4'>What I&apos;ve Built</h2>
         <div className='grid md:grid-cols-2 gap-8'>
           
-          <ProjectItem
-            title='Cybersecurity Swizz Army Knife'
-            backgroundImg={TempImg}
-            projectUrl='/csak'
-            tech='React JS'
-          />
-          <div className="flex flex-col justify-center items-center">
-            <h3>Cybersecurity Swizz Army Knife</h3>
+          <div className="flex flex-col justify-center items-center sm:order-1 order-1">
+            <p className="text-xl">Information Security Multitool</p>
             <p>Description</p>
-            <p>BUTTON</p>
+            <Link href={'/infosec-multitool'}>
+              <p className='text-center py-3 rounded-lg bg-white text-gray-700 font-bold text-lg cursor-pointer'>More Info</p>
+            </Link>
           </div>
+          <Image className='rounded-xl group-hover:opacity-10 sm:order-2 order-2' src={TempImg} alt='/' /> 
           
-          
-          <ProjectItem
-            title='Landmass Generator'
-            backgroundImg={TempImg}
-            projectUrl='/generator'
-            tech='React JS'
-          />
-          <div className="flex flex-col justify-center items-center">
+          <div className="flex flex-col justify-center items-center sm:order-4 order-3">
             <h3>Landmass Generator</h3>
             <p>Description</p>
             <p>BUTTON</p>
           </div>
-
+          <Image className='rounded-xl group-hover:opacity-10 sm:order-3 order-4' src={TempImg} alt='/' /> 
           
-          <ProjectItem
-            title='Word Puzzle'
-            backgroundImg={TempImg}
-            projectUrl='/puzzle'
-            tech='React JS'
-          />
-          <div className="flex flex-col justify-center items-center">
+          <div className="flex flex-col justify-center items-center sm:order-5 order-5">
             <h3>Word Puzzle</h3>
             <p>Description</p>
             <p>BUTTON</p>
           </div>
+          <Image className='rounded-xl group-hover:opacity-10 sm:order-6 order-6' src={TempImg} alt='/' /> 
         </div>
       </div>
     </div>
