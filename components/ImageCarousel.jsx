4b48ff29adce6171120488d4d4d1b0921bc8977f
daniel-macias <josedanielmr@gmail.com>
2024-06-18 00:34:55 -0600

    added autoplay to image carousel

--- components/ImageCarousel.jsx
+++ components/ImageCarousel.jsx
@@ -1,12 +1,13 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useState, useEffect } from 'react';
 import useEmblaCarousel from 'embla-carousel-react';
 import Image from 'next/image';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
 
-const ImageCarousel = ({ images }) => {
+const ImageCarousel = ({ images, autoplayInterval = 3000 }) => {
   const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true });
   const [selectedIndex, setSelectedIndex] = useState(0);
+  const [isPlaying, setIsPlaying] = useState(true);
 
   const scrollPrev = useCallback(() => {
     if (emblaApi) emblaApi.scrollPrev();
@@ -21,44 +22,57 @@ const ImageCarousel = ({ images }) => {
     setSelectedIndex(emblaApi.selectedScrollSnap());
   }, [emblaApi]);
 
-  // Setup emblaApi event listeners
-  React.useEffect(() => {
+  useEffect(() => {
     if (!emblaApi) return;
     emblaApi.on("select", onSelect);
     onSelect(); // Initial selection
     return () => emblaApi.off("select", onSelect);
   }, [emblaApi, onSelect]);
 
+  useEffect(() => {
+    const autoplay = () => {
+      if (emblaApi && isPlaying) {
+        emblaApi.scrollNext();
+      }
+    };
+
+    const autoplayId = setInterval(autoplay, autoplayInterval);
+
+    return () => clearInterval(autoplayId);
+  }, [emblaApi, isPlaying, autoplayInterval]);
+
   return (
-    <div className="embla">
-      <div className="embla__viewport" ref={emblaRef}>
-        <div className="embla__container">
+    <div 
+      className="relative overflow-hidden max-w-full"
+      onMouseEnter={() => setIsPlaying(false)}
+      onMouseLeave={() => setIsPlaying(true)}
+    >
+      <div className="overflow-hidden" ref={emblaRef}>
+        <div className="flex">
           {images.map((src, index) => (
-            <div className="embla__slide" key={index}>
-              <Image src={src} alt={`Slide ${index}`} objectFit="contain" />
+            <div className="relative flex-[0_0_100%]" key={index}>
+              <Image src={src} alt={`Slide ${index}`}  objectFit="contain" />
             </div>
           ))}
         </div>
       </div>
-      {/* Left Arrow */}
       <FontAwesomeIcon 
         icon={faChevronLeft} 
         onClick={scrollPrev} 
         className="absolute top-1/2 left-5 -translate-y-1/2 z-20 p-3 bg-black bg-opacity-40 text-white cursor-pointer rounded-full" 
         size="lg" 
       />
-
       <FontAwesomeIcon 
         icon={faChevronRight} 
         onClick={scrollNext} 
         className="absolute top-1/2 right-5 -translate-y-1/2 z-20 p-3 bg-black bg-opacity-40 text-white cursor-pointer rounded-full" 
         size="lg" 
       />
-      <div className="embla__dots">
+      <div className="flex justify-center mt-2">
         {images.map((_, index) => (
           <button
             key={index}
-            className={`embla__dot ${index === selectedIndex ? "is-selected" : ""}`}
+            className={`w-3 h-3 rounded-full mx-1 transition-colors ${index === selectedIndex ? "bg-gray-800" : "bg-gray-400"}`}
             onClick={() => emblaApi && emblaApi.scrollTo(index)}
           />
         ))}
